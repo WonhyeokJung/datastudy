16b8cf512072d1b26a9446830af7c0f43f721b3a
Won-hyeok Jung <wonhyeok.contact@gmail.com>
2021-08-31 17:59:59 +0900

    2nd commit

---
+++ BookData.py
@@ -0,0 +1,14 @@
+import json
+import xmltodict
+import requests
+
+url = 'http://data4library.kr/api/srchDtlList?authKey=96f1954ac46690b179fea20e2e37a24101532a730dc0f1a56432925846826f2b&isbn13=9788983921987&loaninfoYN=Y'
+
+res = requests.get(url)
+if res.status_code == 200:
+    data = xmltodict.parse(res.text)
+
+    with open('bookData.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent='\t', ensure_ascii=False)
+    # json_data = json.dumps(data)
+    # print(json_data)
